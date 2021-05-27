32c79c3dbe6b9ea9ed3d1665e1c44229048d4d88
Eric D. Torres <eric.d.torres@ibm.com>
2021-05-27 12:21:38 -0500

    cart context working

--- src/store/cart-provider.js
+++ src/store/cart-provider.js
@@ -1,13 +1,40 @@
+import { useReducer } from "react";
 import CartContext from "./cart-context";
 
-const CartProvider = (props) => {
-  const addItemToCarthandler = (item) => {};
+const initialCartState = {
+  items: [],
+  totalAmount: 0,
+};
 
-  const removeItemFromCartHandler = (id) => {};
+const cartReducer = (state, action) => {
+  if (action.type === "ADD_TO_CART") {
+    const updatedItems = state.items.concat(action.item);
+    const newTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: newTotalAmount,
+    };
+  }
+  if (action.type === "REMOVE FROM") {
+  }
+  return initialCartState;
+};
+
+const CartProvider = (props) => {
+  const [cartState, dispatchCart] = useReducer(cartReducer, initialCartState);
+
+  const addItemToCarthandler = (item) => {
+    dispatchCart({ type: "ADD_TO_CART", item });
+  };
+
+  const removeItemFromCartHandler = (id) => {
+    dispatchCart({ type: "REMOVE_FROM_CART", id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCarthandler,
     removeItem: removeItemFromCartHandler,
   };
